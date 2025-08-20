096941252e36633a6e48e30ef9431ac8f8b8827d
Anaaaraujo1 <anaclaraaraujo0409@gmail.com>
2025-08-20 18:32:29 -0300

    Add files via upload

---
+++ adivinhacao.py
@@ -0,0 +1,9 @@
+import random
+
+numeroAleatorio =  random .randint(1 ,10) 
+palpiteJogador = int (input("digite um numero imteiro de 1 a 10:" ))
+numeroTentativas =1
+
+
+# while numeroAleatorio != palpiteJogador:
+                     
